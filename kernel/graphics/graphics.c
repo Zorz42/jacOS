3ce7794e54ef0d21b3bb4edbf5854f6324a8acda
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2021-04-12 12:54:36 +0200

    trying to improve graphics library

--- kernel/graphics/graphics.c
+++ kernel/graphics/graphics.c
@@ -3,24 +3,20 @@
 
 void kernel_main();
 
+#define DRAW_BUFFER_BYTES_PER_PIXEL sizeof(unsigned int)
+
 mode_info_t* mode_info;
 unsigned char* buffer1;
-unsigned char* buffer2;
-unsigned long long* buffer1_long;
-unsigned long long* buffer2_long;
-int bytes_per_pixel;
-int pixels_in_bytes;
+unsigned int* buffer2;
+int total_pixels;
 
 void putpixel(int x, int y, unsigned char r, unsigned char g, unsigned char b) {
-    unsigned where = x * bytes_per_pixel + y * mode_info->resolutionX * bytes_per_pixel;
-    buffer2[where] = b;
-    buffer2[where + 1] = g;
-    buffer2[where + 2] = r;
+    buffer2[x + y * mode_info->resolutionX] = b + (g << 8) + (r << 16);
 }
 
 void swapBuffers() {
-    for(int i = 0; i < pixels_in_bytes; i++)
-        buffer1_long[i] = buffer2_long[i];
+    for(int i = 0; i < total_pixels; i++)
+        *((unsigned int*)(buffer1 + i * 3)) = buffer2[i];
 }
 
 int getScreenWidth() {
@@ -34,12 +30,9 @@ int getScreenHeight() {
 void graphics_entry(mode_info_t* mode_info_) {
     mode_info = mode_info_;
     
+    total_pixels = mode_info->resolutionX * mode_info->resolutionY;
     buffer1 = (unsigned char*)mode_info->buffer;
-    buffer2 = (unsigned char*)mode_info->buffer + mode_info->resolutionX * mode_info->resolutionY * mode_info->bpp / 8;
-    buffer1_long = (unsigned long*) buffer1;
-    buffer2_long = (unsigned long*) buffer2;
-    bytes_per_pixel = mode_info->bpp / 8;
-    pixels_in_bytes = mode_info->resolutionX * mode_info->resolutionY * bytes_per_pixel / 8;
+    buffer2 = (unsigned int*)((unsigned char*)mode_info->buffer + total_pixels * mode_info->bpp / 8);
     
     kernel_main();
 }
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,14 +2,14 @@
 
 void kernel_main() {
     for(int i = 0; i < 30; i++) {
-        for(int x = 0; x < getScreenWidth(); x++)
-            for(int y = 0; y < getScreenHeight(); y++)
+        for(int x = 0; x < getScreenWidth() - 20; x++)
+            for(int y = 0; y < getScreenHeight() - 20; y++)
                 putpixel(x, y, 255, 100, 255);
         
         swapBuffers();
         
-        for(int x = 0; x < getScreenWidth(); x++)
-            for(int y = 0; y < getScreenHeight(); y++)
+        for(int x = 0; x < getScreenWidth() - 20; x++)
+            for(int y = 0; y < getScreenHeight() - 20; y++)
                 putpixel(x, y, 100, 255, 100);
         
         swapBuffers();
